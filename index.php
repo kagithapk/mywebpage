44e4261d02e5f0c080fcfd682b71d27001de8d66
Praveen Kagitha <kagithapk@gmail.com>
2019-02-08 19:58:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>
+            Home!
+        </title>
+    </head>
+    <body>
+        <form method="POST" action="smsapi.php">
+            <input type="submit" name="Click!" value="Click!">
+    </body>
+</html>
